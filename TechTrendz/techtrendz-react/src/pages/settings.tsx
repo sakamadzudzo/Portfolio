447e61cfe4184a58f8b7e9a1edc42eac6e838ed2
Saka Shingirai Madzudzo <sakamadzudzo@gmail.com>
2024-10-17 17:06:49 +0200

    settings page is only for administrators
    Change so that sections instead are shown based on roles

--- TechTrendz/techtrendz-react/src/pages/settings.tsx
+++ TechTrendz/techtrendz-react/src/pages/settings.tsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux"
 import { AuthState } from "../components/utils/authSlice"
 import { Role } from "../types/types"
-import { useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { OverlayContextType } from "../components/Layout";
 import { useEffect } from "react";
 
 export const Settings = () => {
     const { setLoading, setEmpty } = useOutletContext<OverlayContextType>();
     const roles = useSelector((state: AuthState) => state.auth ? state.auth.roles : [] as Role[])
+    const navigate = useNavigate()
 
     // setLoading(false)
     // setEmpty(false)
@@ -16,10 +17,19 @@ export const Settings = () => {
         document.title = 'TechBrandz - Home';
     }, []);
 
+    useEffect(() => {
+        if (!roles?.find((role) => role.name.toUpperCase() === 'ADMINISTRATOR')) {
+            navigate("/")
+        }
+    }, [navigate, roles])
+
     return (
-        <div className="wrapper">
-            <div>{roles && roles[0].name}</div>
-            <div className="btn-hollow">Remove orphaned files</div>
+        <div className="wrapper flex-col">
+            <div>{roles && roles[2].name}</div>
+            <section className="">
+                <h1>File Management</h1>
+                <div className="btn-hollow w-fit px-3">Remove orphaned files</div>
+            </section>
         </div>
     )
 }
